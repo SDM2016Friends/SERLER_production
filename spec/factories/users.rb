02517be3fd6a7eaea2b0ade69badaca1f93716e8
Johnny Jiang <Johnny@DebugJ.local>
2016-10-04 15:07:06 +1300

    Fixed the issue of Rspc caused by database changes.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,8 @@ FactoryGirl.define do
   factory :user do |f|
     f.email { Faker::Internet.email }
     f.password { Faker::Internet.password(8) }
+    f.first_name {Faker::Name.name}
+    f.middle_name {Faker::Name.name}
+    f.last_name {Faker::Name.name}
   end
 end
