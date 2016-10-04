11ebb2ca5b29394b07e5f5ef6a927c2e585c1786
Johnny Jiang <Johnny@DebugJ.local>
2016-10-04 15:44:03 +1300

    Fixed the issue of Rspec testing caused by database changes.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,8 @@ FactoryGirl.define do
     email { Faker::Internet.email  }
     password "password"
     password_confirmation "password"
+    first_name { Faker::Name.name }
+    middle_name { Faker::Name.name }
+    last_name { Faker::Name.name }
   end
 end
