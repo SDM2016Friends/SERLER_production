9b286b9ea9497e083e99c33d52329d61c323882a
Johnny <johnnyj0622@yahoo.com>
2016-10-16 23:09:35 +0000

    fix the config file of product enviornment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,6 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'https://serler-production-johnnyj-1.c9users.io' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = true
